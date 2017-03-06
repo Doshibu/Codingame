3a135a1be707daf850c6718ab1dc18fb3ee80cf9
Doshibu <guillaume.bonhommeau@viacesi.fr>
2017-03-06 20:59:12 +0100

    Removed Python3 from Repo. Whole Structure Update.

--- Python3/Facile/the-descent.py
+++
@@ -1,9 +0,0 @@
-import sys
-import math
-
-while True:
-    keys={}
-    for i in range(8):
-        mountain_h = int(input())
-        keys[i]=mountain_h
-        if i==7 : print(max(keys, key=keys.get))
