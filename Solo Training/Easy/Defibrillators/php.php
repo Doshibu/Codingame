0dd0ef4271b8034602d4c30e3d4d272dc558e9f2
Doshibu <guillaume.bonhommeau@viacesi.fr>
2017-03-24 21:15:42 +0100

    php horce race

--- Solo Training/Easy/Defibrillators/php.php
+++ Solo Training/Easy/Defibrillators/php.php
@@ -1,17 +1,23 @@
 <?php
-fscanf(STDIN, "%d",$N);
-$list = array();
-for ($i = 0; $i < $N; $i++){
-    fscanf(STDIN, "%d",$Pi);
-    if($Pi>0 && $Pi!==NULL) array_push($list, $Pi);
-}
-
-sort($list);
-$minDiff=$list[0];
-for($i=1; $i<count($list); $i++)
-{
-    $diff=$list[$i]-$list[$i-1];
-    if($diff<$minDiff) $minDiff=$diff;
-}
-echo($minDiff);
-?>
+    fscanf(STDIN, "%s", $LON);
+    fscanf(STDIN, "%s", $LAT);
+    fscanf(STDIN, "%d", $N);
+    function php_type_fix(&$val) {
+        $val = str_replace(',', '.', $val);
+    }
+    php_type_fix($LON);
+    php_type_fix($LAT);
+    $min = $title = null;
+    for ($i = 0; $i < $N; $i++) {
+        $defib = explode(';', stream_get_line(STDIN, 256, "\n"));
+        php_type_fix($defib[4]);
+        php_type_fix($defib[5]);
+        $x = ($defib[4] - $LON) * cos(($LAT + $defib[5]) / 2);
+        $y = $defib[5] - $LAT;
+        $res = sqrt(pow($x, 2) + pow($y, 2)) * 6371;
+        if ($min === null || $min > $res) {
+            $min = $res;
+            $title = $defib[1];
+        }
+    }
+    echo($title . "\n");
---
+++ Solo Training/Easy/Horse Racing Duals/php.php
@@ -0,0 +1,16 @@
+<?php
+fscanf(STDIN, "%d",$N);
+$list = array();
+for ($i = 0; $i < $N; $i++){
+    fscanf(STDIN, "%d",$Pi);
+    if($Pi>0 && $Pi!==NULL) array_push($list, $Pi);
+}
+
+sort($list);
+$minDiff=$list[0];
+for($i=1; $i<count($list); $i++)
+{
+    $diff=$list[$i]-$list[$i-1];
+    if($diff<$minDiff) $minDiff=$diff;
+}
+echo($minDiff);
