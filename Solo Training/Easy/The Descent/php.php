89c6fc91326b19c0132928bc2060b163af4b23f9
Doshibu <guillaume.bonhommeau@viacesi.fr>
2017-03-24 21:57:36 +0100

    php descent

--- Solo Training/Easy/The Descent/php.php
+++ Solo Training/Easy/The Descent/php.php
@@ -1,11 +1,17 @@
 <?php
-    while (true) {
-        fscanf(STDIN, "%d %d", $spaceX, $spaceY);
-        $mountains = [];
-        for ($i = 0; $i < 8; $i++) {
-            fscanf(STDIN, "%d", $mountainH);
-            $mountains[] = $mountainH;
-        }
-        arsort($mountains, SORT_NUMERIC);
-        echo ($spaceX == array_keys($mountains)[0]) ? "FIRE\n" : "HOLD\n";
+// game loop
+while (TRUE){
+    $maisAlta = 0;      //Armazena a montanha mais alta
+    $pos = 0;           //Armazena a posição da montanha mais alta
+    for ($i = 0; $i < 8; $i++){
+        fscanf(STDIN, "%d",
+            $mountainH // represents the height of one mountain, from 9 to 0.
+        );
+        if($mountainH > $maisAlta): //Descobre a montanha mais alta neste turno
+            $pos = $i;
+            $maisAlta = $mountainH;
+        endif;
     }
+    //Atira na montanha mais alta encontrada, perigo iminente
+    echo("{$pos}\n"); // The number of the mountain to fire on.
+}
